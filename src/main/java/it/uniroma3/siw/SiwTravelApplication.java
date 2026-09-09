bdcd8c5d6b170e6c6d69ad94f93c288ee7f2d712
Matteo Grifantini <matteo.grifantini@gmail.com>
2026-09-09 21:52:51 +0200

    setup iniziale progetto

---
+++ src/main/java/it/uniroma3/siw/SiwTravelApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiwTravelApplication.class, args);
+    }
+}
